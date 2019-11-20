e93235147a3780cb7e65832d839eee20a5b8334f
jingwangzai <1564913419@qq.com>
2019-11-20 20:25:51 +0800

    2019-11-20上传

---
+++ dudujwz/dudu-weixin/src/test/java/com/qfjy/project/DuduWeixinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qfjy.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuduWeixinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
